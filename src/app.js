30ab4fcde02ca9b4245849632f2c88e872ff2c18
Adrian <adrianasdfs@gmail.com>
2019-11-16 20:30:42 +0800

    fix linting issue

--- src/app.js
+++ src/app.js
@@ -1,54 +1,50 @@
-'use strict';
-
 const express = require('express');
-const app = express();
-
 const bodyParser = require('body-parser');
+
+const app = express();
 const jsonParser = bodyParser.json();
 
 module.exports = (db) => {
-
-    /**
+  /**
      * @api {get} health
      * @apiDescription Get API Status
      * @apiVersion 0.0.1
-     * @apiName Status
+     * @apiName Health Status
      * @apiGroup Status
      *
      * @apiSuccess {String} Message
-     * 
-    **/
+  */
 
-    app.get('/health', (req, res) => res.send('Healthy'));
+  app.get('/health', (req, res) => res.send('Healthy'));
 
-    /**
+  /**
      * @api {post} rides Create new Ride
      * @apiDescription Create new Ride
      * @apiVersion 0.0.1
      * @apiName Post Ride
      * @apiGroup Ride
-     * 
+     *
      * @apiParam (Body) {Number} start_lat Ride Origin Latitude.
-     * @apiParam (Body) {Number} start_long Ride Origin Longtitude.  
+     * @apiParam (Body) {Number} start_long Ride Origin Longtitude.
      * @apiParam (Body) {Number} end_lat Ride Destination Latitude.
      * @apiParam (Body) {Number} end_long  Ride Destination Longtitude.
      * @apiParam (Body) {String} rider_name Rider Name.
      * @apiParam (Body) {String} driver_name Driver Name.
      * @apiParam (Body) {String} driver_vehicle Driver Vehicle.
-     
-     * @apiSuccess {Object[]} rides List of Rides 
+     *
+     * @apiSuccess {Object[]} rides List of Rides
      * @apiSuccess {Number} rides.rideID Unique Rider ID
-     * @apiSuccess {Number} rides.startLat Ride Origin Latitude  
-     * @apiSuccess {Number} rides.startLong Ride Origin Longtitude  
-     * @apiSuccess {Number} rides.endLat Ride Destination Latitude  
-     * @apiSuccess {Number} rides.endLong Ride Destination Longtitude      
+     * @apiSuccess {Number} rides.startLat Ride Origin Latitude
+     * @apiSuccess {Number} rides.startLong Ride Origin Longtitude
+     * @apiSuccess {Number} rides.endLat Ride Destination Latitude
+     * @apiSuccess {Number} rides.endLong Ride Destination Longtitude
      * @apiSuccess {String} rides.riderName Rider Name
      * @apiSuccess {String} rides.driverName Driver Name
      * @apiSuccess {String} rides.driverVehicle Driver Vehicle type
-     * 
+     *
      * @apiError (Error) {String} error_code Error Code
      * @apiError (Error) {String} message Error Message
-     * 
+     *
      * @apiSuccessExample Success-Response:
      *
      * HTTP/1.1 200
@@ -57,8 +53,8 @@ module.exports = (db) => {
      *   {
      *      rideId: 1,
      *      startLat: 89,
-     *      startLong: 150, 
-     *      endLat: 89, 
+     *      startLong: 150,
+     *      endLat: 89,
      *      endLong: 150,
      *      riderName: 'rider',
      *      driverName: 'driver',
@@ -70,14 +66,18 @@ module.exports = (db) => {
      *
      *  {
      *    error_code: 'VALIDATION_ERROR'
-     *    message: 'Start latitude and longitude must be between -90 - 90 and -180 to 180 degrees respectively'
+     *    message: '
+     *      Start latitude & longitude must be between -90 - 90 & -180 to 180 degrees respectively
+     *    '
      *  }
      *
      *  @apiErrorExample {json} Invalid Destination Longtitude/Latitude
      *
      *  {
      *    error_code: 'VALIDATION_ERROR'
-     *    message: 'End latitude and longitude must be between -90 - 90 and -180 to 180 degrees respectively'
+     *    message: '
+     *      End latitude & longitude must be between -90 - 90 & -180 to 180 degrees respectively
+     *    '
      *  }
      *
      *  @apiErrorExample {json} Invalid Rider Name
@@ -93,7 +93,7 @@ module.exports = (db) => {
      *    error_code: 'VALIDATION_ERROR'
      *    message: 'Driver name must be a non empty string
      *  }
-     * 
+     *
     *  @apiErrorExample {json} Invalid Driver's Vehicle
      *
      *  {
@@ -110,74 +110,87 @@ module.exports = (db) => {
      *
      */
 
-    app.post('/rides', jsonParser, (req, res) => {
-        const startLatitude = Number(req.body.start_lat);
-        const startLongitude = Number(req.body.start_long);
-        const endLatitude = Number(req.body.end_lat);
-        const endLongitude = Number(req.body.end_long);
-        const riderName = req.body.rider_name;
-        const driverName = req.body.driver_name;
-        const driverVehicle = req.body.driver_vehicle;
+  app.post('/rides', jsonParser, (req, res) => {
+    const startLatitude = Number(req.body.start_lat);
+    const startLongitude = Number(req.body.start_long);
+    const endLatitude = Number(req.body.end_lat);
+    const endLongitude = Number(req.body.end_long);
+    const riderName = req.body.rider_name;
+    const driverName = req.body.driver_name;
+    const driverVehicle = req.body.driver_vehicle;
 
-        if (startLatitude < -90 || startLatitude > 90 || startLongitude < -180 || startLongitude > 180) {
-            return res.send({
-                error_code: 'VALIDATION_ERROR',
-                message: 'Start latitude and longitude must be between -90 - 90 and -180 to 180 degrees respectively'
-            });
-        }
+    const isInvalidStartLatitude = startLatitude < -90 || startLatitude > 90;
+    const isInvalidStartLongtitude = startLongitude < -180 || startLongitude > 180;
 
-        if (endLatitude < -90 || endLatitude > 90 || endLongitude < -180 || endLongitude > 180) {
-            return res.send({
-                error_code: 'VALIDATION_ERROR',
-                message: 'End latitude and longitude must be between -90 - 90 and -180 to 180 degrees respectively'
-            });
-        }
+    if (isInvalidStartLatitude || isInvalidStartLongtitude) {
+      return res.send({
+        error_code: 'VALIDATION_ERROR',
+        message: 'Start latitude and longitude must be between -90 - 90 and -180 to 180 degrees respectively',
+      });
+    }
 
-        if (typeof riderName !== 'string' || riderName.length < 1) {
-            return res.send({
-                error_code: 'VALIDATION_ERROR',
-                message: 'Rider name must be a non empty string'
-            });
-        }
+    if (endLatitude < -90 || endLatitude > 90 || endLongitude < -180 || endLongitude > 180) {
+      return res.send({
+        error_code: 'VALIDATION_ERROR',
+        message: 'End latitude and longitude must be between -90 - 90 and -180 to 180 degrees respectively',
+      });
+    }
 
-        if (typeof driverName !== 'string' || driverName.length < 1) {
-            return res.send({
-                error_code: 'VALIDATION_ERROR',
-                message: 'Driver name must be a non empty string'
-            });
-        }
+    if (typeof riderName !== 'string' || riderName.length < 1) {
+      return res.send({
+        error_code: 'VALIDATION_ERROR',
+        message: 'Rider name must be a non empty string',
+      });
+    }
 
-        if (typeof driverVehicle !== 'string' || driverVehicle.length < 1) {
-            return res.send({
-                error_code: 'VALIDATION_ERROR',
-                message: 'Vehicle type must be a non empty string'
-            });
-        }
+    if (typeof driverName !== 'string' || driverName.length < 1) {
+      return res.send({
+        error_code: 'VALIDATION_ERROR',
+        message: 'Driver name must be a non empty string',
+      });
+    }
 
-        var values = [req.body.start_lat, req.body.start_long, req.body.end_lat, req.body.end_long, req.body.rider_name, req.body.driver_name, req.body.driver_vehicle];
-        
-        const result = db.run('INSERT INTO Rides(startLat, startLong, endLat, endLong, riderName, driverName, driverVehicle) VALUES (?, ?, ?, ?, ?, ?, ?)', values, function (err) {
-            if (err) {
-                return res.send({
-                    error_code: 'SERVER_ERROR',
-                    message: 'Unknown error'
-                });
-            }
+    if (typeof driverVehicle !== 'string' || driverVehicle.length < 1) {
+      return res.send({
+        error_code: 'VALIDATION_ERROR',
+        message: 'Vehicle type must be a non empty string',
+      });
+    }
 
-            db.all('SELECT * FROM Rides WHERE rideID = ?', this.lastID, function (err, rows) {
-                if (err) {
-                    return res.send({
-                        error_code: 'SERVER_ERROR',
-                        message: 'Unknown error'
-                    });
-                }
+    const values = [
+      req.body.start_lat,
+      req.body.start_long,
+      req.body.end_lat,
+      req.body.end_long,
+      req.body.rider_name,
+      req.body.driver_name,
+      req.body.driver_vehicle,
+    ];
 
-                res.send(rows);
-            });
+    const insertQuery = 'INSERT INTO Rides(startLat, startLong, endLat, endLong, riderName, driverName, driverVehicle) VALUES (?, ?, ?, ?, ?, ?, ?)';
+
+    return db.run(insertQuery, values, (err) => {
+      if (err) {
+        return res.send({
+          error_code: 'SERVER_ERROR',
+          message: 'Unknown error',
         });
-    });
+      }
 
-    /**
+      return db.all('SELECT * FROM Rides WHERE rideID = ?', this.lastID, (selectError, rows) => {
+        if (selectError) {
+          return res.send({
+            error_code: 'SERVER_ERROR',
+            message: 'Unknown error',
+          });
+        }
+
+        return res.send(rows);
+      });
+    });
+  });
+
+  /**
      * @api {get} rides Get All Rides
      * @apiDescription Get All Rides
      * @apiVersion 0.0.1
@@ -186,89 +199,14 @@ module.exports = (db) => {
      *
      * @apiSuccess {Object[]} rides List of Rides
      * @apiSuccess {Number} rides.rideID Unique Rider ID
-     * @apiSuccess {Number} rides.startLat Ride Origin Latitude  
-     * @apiSuccess {Number} rides.startLong Ride Origin Longtitude  
-     * @apiSuccess {Number} rides.endLat Ride Destination Latitude  
-     * @apiSuccess {Number} rides.endLong Ride Destination Longtitude      
+     * @apiSuccess {Number} rides.startLat Ride Origin Latitude
+     * @apiSuccess {Number} rides.startLong Ride Origin Longtitude
+     * @apiSuccess {Number} rides.endLat Ride Destination Latitude
+     * @apiSuccess {Number} rides.endLong Ride Destination Longtitude
      * @apiSuccess {String} rides.riderName Rider Name
      * @apiSuccess {String} rides.driverName Driver Name
      * @apiSuccess {String} rides.driverVehicle Driver Vehicle type
-     * 
-     * @apiError (Error) {String} error_code Error Code
-     * @apiError (Error) {String} message Error Message 
-     * 
-     * @apiSuccessExample Success-Response:
      *
-     * HTTP/1.1 200
-     *
-     * [
-     *   {
-     *      rideId: 1,
-     *      startLat: 89,
-     *      startLong: 150, 
-     *      endLat: 89, 
-     *      endLong: 150,
-     *      riderName: 'rider',
-     *      driverName: 'driver',
-     *      driverVehicle: 'car',
-     *   }
-     * ]
-     *
-     * @apiErrorExample {json} Server Error
-     *
-     *  {
-     *    error_code: 'SERVER_ERROR'
-     *    message: 'Unknown error'
-     *  }
-     *
-     * @apiErrorExample {json} Empty Data
-     *
-     *  {
-     *    error_code: 'RIDES_NOT_FOUND_ERROR',
-     *    message: 'Could not find any rides'
-     *  }
-     *
-     */
-
-    app.get('/rides', (req, res) => {
-        db.all('SELECT * FROM Rides', function (err, rows) {
-            if (err) {
-                return res.send({
-                    error_code: 'SERVER_ERROR',
-                    message: 'Unknown error'
-                });
-            }
-
-            if (rows.length === 0) {
-                return res.send({
-                    error_code: 'RIDES_NOT_FOUND_ERROR',
-                    message: 'Could not find any rides'
-                });
-            }
-
-            res.send(rows);
-        });
-    });
-
-    /**
-     * @api {get} rides/:id Get a Ride data
-     * @apiDescription Get a Ride data
-     * @apiVersion 0.0.1
-     * @apiName Get Ride
-     * @apiGroup Ride
-     *
-     * @apiParam {Number} id Ride ID
-     * 
-     * @apiSuccess {Object[]} rides List of Rides 
-     * @apiSuccess {Number} rides.rideID Unique Rider ID
-     * @apiSuccess {Number} rides.startLat Ride Origin Latitude  
-     * @apiSuccess {Number} rides.startLong Ride Origin Longtitude  
-     * @apiSuccess {Number} rides.endLat Ride Destination Latitude  
-     * @apiSuccess {Number} rides.endLong Ride Destination Longtitude      
-     * @apiSuccess {String} rides.riderName Rider Name
-     * @apiSuccess {String} rides.driverName Driver Name
-     * @apiSuccess {String} rides.driverVehicle Driver Vehicle type
-     * 
      * @apiError (Error) {String} error_code Error Code
      * @apiError (Error) {String} message Error Message
      *
@@ -280,8 +218,8 @@ module.exports = (db) => {
      *   {
      *      rideId: 1,
      *      startLat: 89,
-     *      startLong: 150, 
-     *      endLat: 89, 
+     *      startLong: 150,
+     *      endLat: 89,
      *      endLong: 150,
      *      riderName: 'rider',
      *      driverName: 'driver',
@@ -305,25 +243,100 @@ module.exports = (db) => {
      *
      */
 
-    app.get('/rides/:id', (req, res) => {
-        db.all(`SELECT * FROM Rides WHERE rideID='${req.params.id}'`, function (err, rows) {
-            if (err) {
-                return res.send({
-                    error_code: 'SERVER_ERROR',
-                    message: 'Unknown error'
-                });
-            }
-
-            if (rows.length === 0) {
-                return res.send({
-                    error_code: 'RIDES_NOT_FOUND_ERROR',
-                    message: 'Could not find any rides'
-                });
-            }
-
-            res.send(rows);
+  app.get('/rides', (req, res) => {
+    db.all('SELECT * FROM Rides', (err, rows) => {
+      if (err) {
+        return res.send({
+          error_code: 'SERVER_ERROR',
+          message: 'Unknown error',
         });
-    });
+      }
 
-    return app;
+      if (rows.length === 0) {
+        return res.send({
+          error_code: 'RIDES_NOT_FOUND_ERROR',
+          message: 'Could not find any rides',
+        });
+      }
+
+      return res.send(rows);
+    });
+  });
+
+  /**
+     * @api {get} rides/:id Get a Ride data
+     * @apiDescription Get a Ride data
+     * @apiVersion 0.0.1
+     * @apiName Get Ride
+     * @apiGroup Ride
+     *
+     * @apiParam {Number} id Ride ID
+     *
+     * @apiSuccess {Object[]} rides List of Rides
+     * @apiSuccess {Number} rides.rideID Unique Rider ID
+     * @apiSuccess {Number} rides.startLat Ride Origin Latitude
+     * @apiSuccess {Number} rides.startLong Ride Origin Longtitude
+     * @apiSuccess {Number} rides.endLat Ride Destination Latitude
+     * @apiSuccess {Number} rides.endLong Ride Destination Longtitude
+     * @apiSuccess {String} rides.riderName Rider Name
+     * @apiSuccess {String} rides.driverName Driver Name
+     * @apiSuccess {String} rides.driverVehicle Driver Vehicle type
+     *
+     * @apiError (Error) {String} error_code Error Code
+     * @apiError (Error) {String} message Error Message
+     *
+     * @apiSuccessExample Success-Response:
+     *
+     * HTTP/1.1 200
+     *
+     * [
+     *   {
+     *      rideId: 1,
+     *      startLat: 89,
+     *      startLong: 150,
+     *      endLat: 89,
+     *      endLong: 150,
+     *      riderName: 'rider',
+     *      driverName: 'driver',
+     *      driverVehicle: 'car',
+     *   }
+     * ]
+     *
+     * @apiErrorExample {json} Server Error
+     *
+     *  {
+     *    error_code: 'SERVER_ERROR'
+     *    message: 'Unknown error'
+     *  }
+     *
+     * @apiErrorExample {json} Empty Data
+     *
+     *  {
+     *    error_code: 'RIDES_NOT_FOUND_ERROR',
+     *    message: 'Could not find any rides'
+     *  }
+     *
+     */
+
+  app.get('/rides/:id', (req, res) => {
+    db.all(`SELECT * FROM Rides WHERE rideID='${req.params.id}'`, (err, rows) => {
+      if (err) {
+        return res.send({
+          error_code: 'SERVER_ERROR',
+          message: 'Unknown error',
+        });
+      }
+
+      if (rows.length === 0) {
+        return res.send({
+          error_code: 'RIDES_NOT_FOUND_ERROR',
+          message: 'Could not find any rides',
+        });
+      }
+
+      return res.send(rows);
+    });
+  });
+
+  return app;
 };
