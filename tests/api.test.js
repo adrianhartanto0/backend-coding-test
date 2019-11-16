af404d145fbd45b4e1b01a2726f1e6bd4a169830
Adrian <adrianasdfs@gmail.com>
2019-11-16 22:38:48 +0800

    add test for GET /rides to response payload is correct if server error occurs

--- tests/api.test.js
+++ tests/api.test.js
@@ -1,5 +1,7 @@
 
 const request = require('supertest');
+const sinon = require('sinon');
+const { expect } = require('chai');
 const sqlite3 = require('sqlite3').verbose();
 const buildSchemas = require('../src/schemas');
 
@@ -33,6 +35,23 @@ describe('API tests', () => {
       request(app)
         .get('/rides')
         .expect(200, done);
+    });
+
+    it('If an error occurs retrieving rides, response must contain corrent payload', (done) => {
+      sinon.stub(db, 'all').yieldsRight(new Error('error'));
+
+      request(app)
+        .get('/rides')
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .then((response) => {
+          expect(Object.keys(response.body).length).to.be.equal(2);
+          expect(response.body).to.have.property('error_code');
+          expect(response.body).to.have.property('message');
+          expect(response.body.error_code).to.equal('SERVER_ERROR');
+          expect(response.body.message).to.equal('Unknown error');
+          done();
+        });
     });
   });
 });
