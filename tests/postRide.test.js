cb8c22ca76ecbba58da400bd269aa0af4e745831
Adrian <adrianasdfs@gmail.com>
2019-11-16 23:54:49 +0800

    add test for POST /rides, check if response contains correct payload if start latitude/longtitude is invalid

--- tests/postRide.test.js
+++ tests/postRide.test.js
@@ -1,4 +1,6 @@
 const request = require('supertest');
+const Chance = require('chance')();
+const { expect } = require('chai');
 
 const sqlite3 = require('sqlite3').verbose();
 
@@ -10,5 +12,37 @@ describe('POST /rides', () => {
     request(app)
       .post('/rides')
       .expect(200, done);
+  });
+
+  it('If rider start latitude is invalid, response must contain correct error payload', (done) => {
+    const sampleInvalidLatitude = Chance.integer({ min: 100000, max: 150000 });
+
+    request(app)
+      .post('/rides')
+      .send({ start_lat: sampleInvalidLatitude })
+      .expect(200)
+      .then((response) => {
+        expect(response.body).to.have.property('error_code');
+        expect(response.body).to.have.property('message');
+        expect(response.body.error_code).to.equal('VALIDATION_ERROR');
+        expect(response.body.message).to.equal('Start latitude and longitude must be between -90 - 90 and -180 to 180 degrees respectively');
+        done();
+      });
+  });
+
+  it('If rider start longtitude is invalid, response must contain correct error payload', (done) => {
+    const sampleInvalidLongtitude = Chance.integer({ min: 100000, max: 150000 });
+
+    request(app)
+      .post('/rides')
+      .send({ start_lat: sampleInvalidLongtitude })
+      .expect(200)
+      .then((response) => {
+        expect(response.body).to.have.property('error_code');
+        expect(response.body).to.have.property('message');
+        expect(response.body.error_code).to.equal('VALIDATION_ERROR');
+        expect(response.body.message).to.equal('Start latitude and longitude must be between -90 - 90 and -180 to 180 degrees respectively');
+        done();
+      });
   });
 });
