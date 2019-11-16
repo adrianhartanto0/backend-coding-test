81db74372fbdbec1a4f4f006c723be68aea313ee
Adrian <adrianasdfs@gmail.com>
2019-11-16 20:34:56 +0800

    fix linting issue

--- index.js
+++ index.js
@@ -1,24 +1,13 @@
-'use strict';
-
-const express = require('express');
-const app = express();
-const port = 8010;
-
-const bodyParser = require('body-parser');
-const jsonParser = bodyParser.json();
-
 const sqlite3 = require('sqlite3').verbose();
+
+const port = 8010;
 const db = new sqlite3.Database(':memory:');
 
+const app = require('./src/app')(db);
 const { logInfo } = require('./src/utils/logger');
-
-
 const buildSchemas = require('./src/schemas');
 
 db.serialize(() => {
-    buildSchemas(db);
-
-    const app = require('./src/app')(db);
-
-    app.listen(port, () => logInfo(`App started and listening on port ${port}`));
+  buildSchemas(db);
+  app.listen(port, () => logInfo(`App started and listening on port ${port}`));
 });
